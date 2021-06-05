b4e9484ff3357a2c70fba518fddb386a82a870f5
andreasjansson <andreas@replicate.ai>
2021-06-04 18:05:58 -0700

    Clean up examples

--- inst-colorization/scripts/prepare_train_box.sh
+++
@@ -1,3 +0,0 @@
-DATASET_DIR=train_data/train2017
-
-python inference_bbox.py --test_img_dir $DATASET_DIR --filter_no_obj
--- rife/build-and-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eux
-
-image="us-docker.pkg.dev/replicate/example-repos/rife"
-docker build --tag "$image" -f docker/Dockerfile .
-docker push "$image"
