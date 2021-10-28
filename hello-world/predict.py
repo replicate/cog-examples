308c0314687f87325c07da696b08fe31f24b9f23
Zeke Sikelianos <zeke@sikelianos.com>
2021-10-28 11:49:04 -0700

    make a cosmetic improvement to hello-world output

--- hello-world/predict.py
+++ hello-world/predict.py
@@ -1,10 +1,9 @@
 import cog
 
-
 class Predictor(cog.Predictor):
     def setup(self):
         self.prefix = "hello"
 
     @cog.input("input", type=str, help="Text that will get prefixed by 'hello '")
     def predict(self, input):
-        return self.prefix + " " + input
+        return f"\n\n{self.prefix} {input}\n\n"
